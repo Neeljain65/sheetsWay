b7061acfd69e4fea6ec0b3d0d2034cee67537ad3
Neel jain <nj6097114@gmail.com>
2024-12-14 23:13:02 +0530

    bug fixed for the jutsofy-conetn

--- src/components/TravelTips.jsx
+++ src/components/TravelTips.jsx
@@ -17,20 +17,20 @@ function TravelTips() {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                       {/* Card 1 */}
                       {tips.map((ln, i) => (
-                        <div key={i} className="shadow-lg rounded-lg overflow-hidden">
+                        <div key={i} className=" flex flex-col p-10  items-center justify-center shadow-lg rounded-lg overflow-hidden">
                           <img
                             src="image.png"
                             alt="Card"
                             className=" object-contain"
                           />
                           <div className="p-6">
-                            <div className="flex gap-2 mb-4">
+                            <div className="flex gap-2 ml-5 mb-4 ">
                               <p className="border-solid border-2 border-gray-500 p-1 rounded-md text-sm px-3">Travel</p>
                               <p className="border-solid border-2 border-gray-500 p-1 rounded-md text-sm px-3">Travel Blog</p>
                               <p className="border-solid border-2 border-gray-500 p-1 rounded-md text-sm px-3">Malta Blog</p>
                            
                             </div>
-                            <h3 className="text-lg font-bold mb-2">{ln.title}</h3>
+                            <h3 className="text-lg font-bold mb-2 ml-3">{ln.title}</h3>
                             <p className="text-gray-700 mb-4">
                             {ln.description}
                             </p>
